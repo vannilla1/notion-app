a9874c1098e049e60dcae11f90e23e7e3a5ac328
Martin Koščo <martinkosco@MacBook-Air.local>
2025-12-19 17:45:31 +0100

    Add express body parser

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Make io accessible to routes
 app.set('io', io);
